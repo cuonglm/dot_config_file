60d779d471839d24d19d2787efb44fa8dc67309a
BOSS14420 <firefoxlinux@gmail.com>
2013-06-25 16:12:16 +0700

    handle header:   myhtml2text.py

--- mdeditor/myhtml2text.py
+++ mdeditor/myhtml2text.py
@@ -29,6 +29,36 @@ def google_fixed_width_font(style):
 html2text.google_fixed_width_font = google_fixed_width_font
 
 
+def hn(ts, ps):
+    """check header
+    ts: tag_style
+    ps: parent_style
+    """
+
+    #if 'margin-bottom' in ts and ts['margin-bottom'] == '0px' \
+    #        and 'margin-top' in ts and ts['margin-top'] == '0px':
+    #    return 6
+
+    if 'margin-top' not in ps or 'margin-bottom' not in ps:
+        return 0
+
+    if 'font-weight' in ts and ts['font-weight'] == '600' \
+            and 'font-size' in ts:
+        if ps['margin-bottom'] == '12px':
+            if ts['font-size'] == 'xx-large' and ps['margin-top'] == '18px':
+                return 1
+            if ts['font-size'] == 'x-large' and ps['margin-top'] == '16px':
+                return 2
+            if ts['font-size'] == 'large' and ps['margin-top'] == '14px':
+                return 3
+            if ts['font-size'] == 'medium' and ps['margin-top'] == '12px':
+                return 4
+        elif ps['margin-bottom'] == '4px' and ps['margin-top'] == '12px' \
+                and ts['font-size'] == 'small':
+            return 5
+    return 0
+
+
 class MyHtml2Text(html2text.HTML2Text):
     def __init__(self, out=None, baseurl=''):
         self.parent_class = html2text.HTML2Text
@@ -68,6 +98,74 @@ class MyHtml2Text(html2text.HTML2Text):
             else:
                 self.o('</' + tag + '>')
                 self.not_in_html_block.pop()
+
+    def handle_emphasis(self, start, tag_style, parent_style):
+        """handles various text emphases"""
+        tag_emphasis = google_text_emphasis(tag_style)
+        parent_emphasis = google_text_emphasis(parent_style)
+
+        # handle Google's text emphasis
+        hl = hn(tag_style, parent_style)
+        strikethrough = 'line-through' in tag_emphasis and self.hide_strikethrough
+        bold = 'bold' in tag_emphasis and not 'bold' in parent_emphasis and hl in {0, 6}
+        italic = 'italic' in tag_emphasis and not 'italic' in parent_emphasis
+        fixed = google_fixed_width_font(tag_style) and not \
+                google_fixed_width_font(parent_style) and not self.pre
+
+        if start:
+            # crossed-out text must be handled before other attributes
+            # in order not to output qualifiers unnecessarily
+            if hl:
+                self.o("#" * hl)
+            if bold or italic or fixed or strikethrough:
+                self.emphasis += 1
+            if strikethrough:
+                #self.quiet += 1
+                self.o("<s>")
+            if italic:
+                self.o("_")
+                self.drop_white_space += 1
+            if bold:
+                self.o("**")
+                self.drop_white_space += 1
+            if fixed:
+                self.o('`')
+                self.drop_white_space += 1
+                self.code = True
+        else:
+            if bold or italic or fixed:
+                # there must not be whitespace before closing emphasis mark
+                self.emphasis -= 1
+                self.space = 0
+                self.outtext = self.outtext.rstrip()
+            if fixed:
+                if self.drop_white_space:
+                    # empty emphasis, drop it
+                    self.drop_last(1)
+                    self.drop_white_space -= 1
+                else:
+                    self.o('`')
+                self.code = False
+            if bold:
+                if self.drop_white_space:
+                    # empty emphasis, drop it
+                    self.drop_last(2)
+                    self.drop_white_space -= 1
+                else:
+                    self.o("**")
+            if italic:
+                if self.drop_white_space:
+                    # empty emphasis, drop it
+                    self.drop_last(1)
+                    self.drop_white_space -= 1
+                else:
+                    self.o("_")
+            # space is only allowed after *all* emphasis marks
+            if (bold or italic) and not self.emphasis:
+                    self.o(" ")
+            if strikethrough:
+                #self.quiet -= 1
+                self.o("</s>")
 
     def google_nest_count(self, style):
         """calculate the nesting count of google doc lists"""
