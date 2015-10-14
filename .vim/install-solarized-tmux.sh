e6fe774a8ee9f279b049193fd0cae8274f08162d
LE Manh Cuong <manhcuongle@hotmail.com>
2015-10-14 10:41:56 +0700

    Refactoring install scripts

--- .vim/install-solarized-tmux.sh
+++ .vim/install-solarized-tmux.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-GIT="$(command -v git)"
-CAT="$(command -v cat)"
+GIT=$(command -v git)
+CAT=$(command -v cat)
+REPO="https://github.com/seebi/tmux-colors-solarized.git"
 
-base_dir="$(dirname -- $0)"
+base_dir=${0%/*}
 
-if [ ! -d "${base_dir}/tmux-colors-solarized" ]
-then
-    "$GIT" clone https://github.com/seebi/tmux-colors-solarized.git
+if [ ! -d "${base_dir}/tmux-colors-solarized" ]; then
+  "$GIT" clone "$REPO"
 fi
 
 case "$1" in
-    (dark) "$CAT" "${base_dir}/tmux-colors-solarized/tmuxcolors-dark.conf" >> ~/.tmux.conf ;;
-   (light) "$CAT" "${base_dir}/tmux-colors-solarized/tmuxcolors-light.conf" >> ~/.tmux.conf ;;
-       (*) "$CAT" "${base_dir}/tmux-colors-solarized/tmuxcolors-256.conf" >> ~/.tmux.conf ;;
+   (dark) "$CAT" "${base_dir}/tmux-colors-solarized/tmuxcolors-dark.conf" >> ~/.tmux.conf ;;
+  (light) "$CAT" "${base_dir}/tmux-colors-solarized/tmuxcolors-light.conf" >> ~/.tmux.conf ;;
+      (*) "$CAT" "${base_dir}/tmux-colors-solarized/tmuxcolors-256.conf" >> ~/.tmux.conf ;;
 esac
--- .vim/install-solarized-xfce.sh
+++ .vim/install-solarized-xfce.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-GIT="$(command -v git)"
-CP="$(command -v cp)"
+GIT=$(command -v git)
+CP=$(command -v cp)
+REPO="https://github.com/sgerrand/xfce4-terminal-colors-solarized.git"
 
 # Config xfce terminal with solarized-theme
 has_xfce=
-base_dir="$(dirname -- $0)"
+base_dir=${0%/*}
 
-case "$DESKTOP_SESSION" in
-    (*xfce*) has_xfce=yes ;;
-         (*) has_xfce= ;;
+case $DESKTOP_SESSION in
+  (*xfce*) has_xfce=yes ;;
+       (*) has_xfce= ;;
 esac
 
 if [ -n "$has_xfce" ] && [ ! -d "${base_dir}/xfce4-terminal-colors-solarized" ]
 then
-    "$GIT" clone https://github.com/sgerrand/xfce4-terminal-colors-solarized.git
+  "$GIT" clone "$REPO"
 fi
 
-case "$1" in
-    (dark) "$CP" "${base_dir}/xfce4-terminal-colors-solarized/dark/terminalrc" ~/.config/Terminal/ ;;
-   (light) "$CP" "${base_dir}/xfce4-terminal-colors-solarized/light/terminalrc" ~/.config/Terminal/ ;;
-       (*) printf "Usage: %s light|dark\n" "$0"
+case $1 in
+   (dark) "$CP" "${base_dir}/xfce4-terminal-colors-solarized/dark/terminalrc" ~/.config/Terminal/ ;;
+  (light) "$CP" "${base_dir}/xfce4-terminal-colors-solarized/light/terminalrc" ~/.config/Terminal/ ;;
+      (*) printf "Usage: %s light|dark\n" "$0"
 esac
