b8acc33b7c43c7505cfbdecd89d76bd0fc90b6c5
LE Manh Cuong <cuong.manhle.vn@gmail.com>
2016-07-18 01:08:31 +0700

    New neobundle clean command

--- .vim/update.sh
+++ .vim/update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-vim -N -u ~/.vim/vimrc_bundle '+NeoBundleInstall!' +qall
+vim -N -u ~/.vim/vimrc_bundle '+NeoBundleInstall!' '+call neobundle#commands#clean(1)' +qall
 [ $? -eq 0 ] && echo 'Update completed!'
