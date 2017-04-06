82aa6b9f5a71f76db8afe1b82b1fd0ced7e1d9f7
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2017-04-06 15:26:07 +0700

    update command for neovim

--- .vim/update.sh
+++ .vim/update.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-vim -N -u ~/.vim/vimrc_bundle '+NeoBundleInstall!' '+call neobundle#commands#clean(1)' +qall
+vim=
+case $1 in
+  (nvim) vim=nvim ;;
+     (*) vim=vim  ;;
+esac
+
+"$vim" -N -u ~/.vim/vimrc_bundle '+NeoBundleInstall!' '+call neobundle#commands#clean(1)' +qall
 [ $? -eq 0 ] && echo 'Update completed!'
